bd7df6e9199fbf5b0c02d6df0182314c0d4a38c7
Diana Shkred <shkred.diana@student.ehu.lt>
2024-10-28 22:55:58 +0300

    HW-1: fix code, delete folder vite-project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemList from './components/ItemList';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>React Homework 1</h1>
+      <ItemList />
+    </>
+  );
+}
+
+export default App;
